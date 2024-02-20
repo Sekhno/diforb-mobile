6f32e8c159d4439d36a3b69ae91b427fb5a62a85
Dmytro Sekhno <dmytro.sekhno@semantrum.net>
2024-02-20 13:01:10 +0200

    DEV using web audio api

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AudioProvider} from "../../providers/audio/audio";
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,6 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public readonly audio: AudioProvider) {}
 
 }
