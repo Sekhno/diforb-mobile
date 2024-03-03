212df5758153b650b609c28a0917c90f17cab961
Dmytro Sekhno <dmytro.sekhno@semantrum.net>
2024-03-03 09:28:44 +0200

    DEV update beginners course, unit 1

---
+++ src/app/courses/beginners-course/units/unit1/examples.ts
@@ -0,0 +1,27 @@
+export const rightSentences = [
+  'I am a soldier',
+  'He is a lawyer',
+  'She is a vet',
+  'We are pilots',
+  'They are builders',
+  'You are a police officer',
+  'She is a shop assistant',
+  'She is a nurse',
+  'He is a hairdresser',
+  'We are cleaners'
+]
+
+export const sentences = [
+  'I {{input}} a soldier.',
+  'He {{input}} a lawyer',
+  'She {{input}} a vet',
+  'We {{input}} pilots',
+  'They {{input}} builders',
+  'You {{input}} a police officer',
+  'She {{input}} a shop assistant',
+  'She {{input}} a nurse',
+  'He {{input}} a hairdresser',
+  'We {{input}} cleaners'
+];
+
+export const rightAnswers = ['am', 'is', 'is', 'are', 'are', 'are', 'is', 'is', 'is', 'are'];
--- src/app/courses/beginners-course/units/unit1/unit1.component.ts
+++ src/app/courses/beginners-course/units/unit1/unit1.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {rightAnswers, rightSentences, sentences} from "./examples";
+import {IonModal} from "@ionic/angular";
 
 @Component({
   selector: 'app-unit1',
   templateUrl: './unit1.component.html',
-  styleUrls: ['./unit1.component.scss'],
+  styleUrls: ['./unit1.component.scss']
 })
 export class Unit1Component  implements OnInit {
+  @ViewChild(IonModal) modal!: IonModal;
+
+  currentExc = 1;
+
+  sentences = sentences;
+  rightAnswers = rightAnswers;
+  rightSentences = rightSentences;
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log('Unit1Component ngOnInit() called.')
+  }
 
 }
