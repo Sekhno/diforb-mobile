60978e4524f57517c7480f8fb5eb9c2e96dfb2ac
Dmytro Sekhno <dmytro.sekhno@semantrum.net>
2024-03-12 14:33:07 +0200

    DEV update unit 2

--- src/app/courses/beginners-course/units/unit2/unit2.component.ts
+++ src/app/courses/beginners-course/units/unit2/unit2.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {IonModal} from "@ionic/angular";
-import {affirmative, vocabulary} from "./examples";
+import {affirmative, negative, question, vocabulary} from "./examples";
 
 @Component({
   selector: 'app-unit2',
@@ -10,12 +10,14 @@ import {affirmative, vocabulary} from "./examples";
 export class Unit2Component {
   @ViewChild(IonModal) modal!: IonModal;
 
-  currentTeo = 1
-  currentExc = 1;
+  currentTeo = 4
+  currentExc = 5;
   progress = 0;
 
   vocabulary = vocabulary;
   affirmative = affirmative;
+  negative = negative;
+  question = question;
 
   constructor() { }
 
